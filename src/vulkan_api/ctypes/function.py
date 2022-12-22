daddafc16902a5e08d682ad0f34d52a26b3e9e53
Plamen Dragiyski <plamen@dragiyski.org>
2022-12-22 16:52:02 +0100

    wip: generated source separation

---
+++ src/vulkan_api/ctypes/function.py
@@ -0,0 +1,6 @@
+if hasattr(ctypes, 'WINFUNCTYPE'):
+    VKAPI_CALL = ctypes.WINFUNCTYPE
+    VKAPI_PTR = ctypes.WINFUNCTYPE
+else:
+    VKAPI_CALL = ctypes.CFUNCTYPE
+    VKAPI_PTR = ctypes.CFUNCTYPE
